beea28a911c4ba76caa4157b4f6d19f8bb6adcfb
e2fo2l <efflam.ollivier@gmail.com>
2023-03-07 10:52:10 +0100

    Added a Transform class

---
+++ src/transform.cpp
@@ -0,0 +1,110 @@
+//    _  __     __   __     ___  ___
+//   / |/ /__ _/ /  / /__ _|_  |/ _ |
+//  /    / _ `/ _ \/ / _ `/ __// // /
+// /_/|_/\_,_/_.__/_/\_,_/____/____/
+//
+// Copyright (C) 2023 - Efflam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "transform.hpp"
+
+#include <glm/gtx/transform.hpp>
+
+namespace nabla2d
+{
+    Transform::Transform(const glm::vec3 &aPosition,
+                         const glm::vec3 &aRotation,
+                         const glm::vec3 &aScale) : mPosition(aPosition),
+                                                    mRotation(aRotation),
+                                                    mScale(aScale),
+                                                    mMatrix(1.0F),
+                                                    mChanged(true)
+    {
+        UpdateMatrix();
+    }
+
+    const glm::vec3 &Transform::GetPosition() const
+    {
+        return mPosition;
+    }
+
+    const glm::vec3 &Transform::GetRotation() const
+    {
+        return mRotation;
+    }
+
+    const glm::vec3 &Transform::GetScale() const
+    {
+        return mScale;
+    }
+
+    void Transform::SetPosition(const glm::vec3 &aPosition)
+    {
+        mPosition = aPosition;
+        mChanged = true;
+    }
+
+    void Transform::SetRotation(const glm::vec3 &aRotation)
+    {
+        mRotation = aRotation;
+        mChanged = true;
+    }
+
+    void Transform::SetScale(const glm::vec3 &aScale)
+    {
+        mScale = aScale;
+        mChanged = true;
+    }
+
+    void Transform::Translate(const glm::vec3 &aTranslation)
+    {
+        mPosition += aTranslation;
+        mChanged = true;
+    }
+
+    void Transform::Rotate(float aAngle, const glm::vec3 &aAxis)
+    {
+        mRotation += aAxis * aAngle;
+        mChanged = true;
+    }
+
+    void Transform::Scale(const glm::vec3 &aScale)
+    {
+        mScale *= aScale;
+        mChanged = true;
+    }
+
+    const glm::mat4 &Transform::GetMatrix()
+    {
+        if (mChanged)
+        {
+            UpdateMatrix();
+        }
+        return mMatrix;
+    }
+
+    void Transform::UpdateMatrix()
+    {
+        mMatrix = glm::translate(glm::mat4(1.0F), mPosition) *
+                  glm::rotate(glm::mat4(1.0F), mRotation.x, glm::vec3(1.0F, 0.0F, 0.0F)) *
+                  glm::rotate(glm::mat4(1.0F), mRotation.y, glm::vec3(0.0F, 1.0F, 0.0F)) *
+                  glm::rotate(glm::mat4(1.0F), mRotation.z, glm::vec3(0.0F, 0.0F, 1.0F)) *
+                  glm::scale(glm::mat4(1.0F), mScale);
+        mChanged = false;
+    }
+
+} // namespace nabla2d
+
+// くコ:彡
---
+++ src/transform.hpp
@@ -0,0 +1,68 @@
+//    _  __     __   __     ___  ___
+//   / |/ /__ _/ /  / /__ _|_  |/ _ |
+//  /    / _ `/ _ \/ / _ `/ __// // /
+// /_/|_/\_,_/_.__/_/\_,_/____/____/
+//
+// Copyright (C) 2023 - Efflam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NABLA2D_TRANSFORM_HPP
+#define NABLA2D_TRANSFORM_HPP
+
+#include <glm/glm.hpp>
+
+namespace nabla2d
+{
+    class Transform
+    {
+    public:
+        Transform(const glm::vec3 &aPosition = {1.0F, 1.0F, 1.0F},
+                  const glm::vec3 &aRotation = {0.0F, 0.0F, 0.0F},
+                  const glm::vec3 &aScale = {1.0F, 1.0F, 1.0F});
+        Transform(const Transform &aTransform) = default;
+        Transform(Transform &&aTransform) = default;
+        Transform &operator=(const Transform &aTransform) = default;
+        ~Transform() = default;
+
+        const glm::vec3 &GetPosition() const;
+        const glm::vec3 &GetRotation() const;
+        const glm::vec3 &GetScale() const;
+
+        void SetPosition(const glm::vec3 &aPosition);
+        void SetRotation(const glm::vec3 &aRotation);
+        void SetScale(const glm::vec3 &aScale);
+
+        void Translate(const glm::vec3 &aTranslation);
+        void Rotate(float aAngle, const glm::vec3 &aAxis);
+        void Scale(const glm::vec3 &aScale);
+
+        const glm::mat4 &GetMatrix();
+
+        void UpdateMatrix();
+
+    private:
+        glm::vec3 mPosition;
+        glm::vec3 mRotation;
+        glm::vec3 mScale;
+
+        glm::mat4 mMatrix;
+
+        bool mChanged;
+    };
+} // namespace nabla2d
+
+#endif // NABLA2D_TRANSFORM_HPP
+
+// くコ:彡
