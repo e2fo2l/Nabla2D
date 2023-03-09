ae67a26865edf413a5f29d1558799ae2a92eed83
e2fo2l <efflam.ollivier@gmail.com>
2023-03-09 10:46:41 +0100

    Add basic Scene object

---
+++ src/scene.cpp
@@ -0,0 +1,108 @@
+//    _  __     __   __     ___  ___
+//   / |/ /__ _/ /  / /__ _|_  |/ _ |
+//  /    / _ `/ _ \/ / _ `/ __// // /
+// /_/|_/\_,_/_.__/_/\_,_/____/____/
+//
+// Copyright (C) 2023 - Efflam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "scene.hpp"
+
+#include "logger.hpp"
+
+namespace nabla2d
+{
+    entt::entity Scene::CreateEntity(const std::string &aTag, entt::entity aParent)
+    {
+        if (aTag == mErrorTag)
+        {
+            Logger::error("Cannot create entity with reserved tag '{}'!", aTag);
+            return entt::null;
+        }
+        if (mTags.find(aTag) != mTags.end())
+        {
+            Logger::error("Cannot create entity with the tag '{}', one with the same tag alread exists!", aTag);
+            return entt::null;
+        }
+
+        auto entity = mRegistry.create();
+        mTags.at(aTag) = entity;
+        mReverseTags.at(entity) = aTag;
+        mParents.at(entity) = aParent;
+
+        return entity;
+    }
+
+    void Scene::DestroyEntity(entt::entity aEntity)
+    {
+        if (aEntity == entt::null)
+        {
+            Logger::error("Cannot delete null entity!");
+            return;
+        }
+
+        if (!mRegistry.valid(aEntity))
+        {
+            Logger::error("Cannot delete entity, it doesn't appear to be valid!");
+        }
+
+        mRegistry.destroy(aEntity);
+        std::string tag = mReverseTags.at(aEntity);
+        mTags.erase(tag);
+        mReverseTags.erase(aEntity);
+        mParents.erase(aEntity);
+    }
+
+    const entt::registry &Scene::GetRegistry() const
+    {
+        return mRegistry;
+    }
+
+    entt::entity Scene::GetEntity(const std::string &aTag) const
+    {
+        auto entity = mTags.find(aTag);
+        if (entity == mTags.end())
+        {
+            Logger::error("No entity with the tag '{}' exists!", aTag);
+            return entt::null;
+        }
+        return entity->second;
+    }
+
+    const std::string &Scene::GetTag(entt::entity aEntity) const
+    {
+        auto tag = mReverseTags.find(aEntity);
+        if (tag == mReverseTags.end())
+        {
+            Logger::error("No entity with the specified id exists!");
+            return mErrorTag;
+        }
+        return tag->second;
+    }
+
+    entt::entity Scene::GetParent(entt::entity aEntity) const
+    {
+        auto parent = mParents.find(aEntity);
+        if (parent == mParents.end())
+        {
+            Logger::error("No entity with the specified id exists!");
+            return entt::null;
+        }
+        return parent->second;
+    }
+
+} // namespace nabla2d
+
+// くコ:彡
---
+++ src/scene.hpp
@@ -0,0 +1,57 @@
+//    _  __     __   __     ___  ___
+//   / |/ /__ _/ /  / /__ _|_  |/ _ |
+//  /    / _ `/ _ \/ / _ `/ __// // /
+// /_/|_/\_,_/_.__/_/\_,_/____/____/
+//
+// Copyright (C) 2023 - Efflam
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NABLA2D_SCENE_HPP
+#define NABLA2D_SCENE_HPP
+
+#include <string>
+#include <unordered_map>
+#include <entt/entt.hpp>
+
+namespace nabla2d
+{
+    class Scene
+    {
+    public:
+        Scene() = default;
+        ~Scene() = default;
+
+        entt::entity CreateEntity(const std::string &aTag, entt::entity aParent = entt::null);
+        void DestroyEntity(entt::entity aEntity);
+
+        const entt::registry &GetRegistry() const;
+        entt::entity GetEntity(const std::string &aTag) const;
+        const std::string &GetTag(entt::entity aEntity) const;
+        entt::entity GetParent(entt::entity aEntity) const;
+
+    private:
+        const std::string mErrorTag{""};
+
+        entt::registry mRegistry;
+        std::unordered_map<std::string, entt::entity> mTags;
+        std::unordered_map<entt::entity, std::string> mReverseTags;
+        std::unordered_map<entt::entity, entt::entity> mParents;
+    };
+
+} // namespace nabla2d
+
+#endif // NABLA2D_SCENE_HPP
+
+// くコ:彡
